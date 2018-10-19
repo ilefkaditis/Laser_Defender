169a87ba5c8250985015f80923078f917bb84139
Ioannis Lefkaditis <ioannis.lefkaditis@spaceapegames.com>
2018-10-19 18:25:51 +0100

    Laser defender work so far

---
+++ Laser Defender/Assets/Scripts/GameSession.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class GameSession : MonoBehaviour {
+
+
+    [Range(0.1f, 2f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int scorePerBlockDestroyed = 83;
+    [SerializeField] int currentScore = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount >1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString(); 
+    }
+
+    public bool IsAytoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
+
+    // Update is called once per frame
+    void Update () 
+    {
+        Time.timeScale = gameSpeed;
+
+    }
+
+    public void AddToScrore()
+    {
+        currentScore += scorePerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
